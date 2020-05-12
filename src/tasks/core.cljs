7f5a2d28dc4d08047f480d0f62a62834ceb80f49
Gabriel Andretta <gabriel.andretta@gmail.com>
2020-05-12 12:36:41 -0300

    Update find-tasks to take a glob pattern

--- src/tasks/core.cljs
+++ src/tasks/core.cljs
@@ -1,14 +1,23 @@
 (ns tasks.core
   (:require [tasks.fs :as fs]
-            [tasks.parser :as p]))
+            [tasks.parser :as p]
+            [clojure.core.async :refer [<! chan close! go put!]]))
 
-(defn find-tasks [path]
-  (-> path
-      fs/read-file
-      p/parse))
+(defn find-tasks-in-files [paths]
+  (let [c (chan)]
+    (doseq [path paths]
+      (put! c {:path path
+               :tasks (-> path fs/read-file p/parse)}))
+    (close! c)
+    c))
+
+(defn find-tasks [pattern]
+  (go (let [c (find-tasks-in-files (<! (fs/ls pattern)))]
+        (loop [xs []]
+          (when-some [x (<! c)]
+            (recur (conj xs x)))
+          xs))))
 
 (comment
   (require '[clojure.pprint :refer [pprint]])
-  (pprint (find-tasks "resources/tasks.md"))
-  (require '[clojure.core.async :refer [<! go]])
-  (go (println (<! (fs/ls "resources/**/*.md")))))
+  (go (pprint (<! (find-tasks "resources/**/*.md")))))
