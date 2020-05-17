885580d7b9a7371efeecd6d776f359d5b090d176
Gabriel Andretta <gabriel.andretta@gmail.com>
2020-05-17 20:01:44 -0300

    Add utility functions to print parser output

--- src/tasks/core.cljs
+++ src/tasks/core.cljs
@@ -18,7 +18,18 @@
             (recur (conj xs x)))
           xs))))
 
+(defn print-tasks [tasks]
+  (doseq [task tasks]
+    (println (:raw task))))
+
+(defn print-node [[{:keys [path raw tasks]} nodes]]
+  (println (or path raw)) ; probably better to handle file and heading node types separately
+  (print-tasks tasks)
+  (doseq [node nodes]
+    (print-node node)))
+
 (comment
   (require '[clojure.pprint :refer [pprint]])
   (go (->> (<! (find-tasks "resources/**/*.md"))
-           pprint)))
+           (map print-node)
+           doall)))
